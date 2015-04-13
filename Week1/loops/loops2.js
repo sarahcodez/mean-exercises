eb18cf0053544106e6d7e8f66058dd3d587d8c2a
sarahcodez <sarahgrahamx@gail.com>
2015-04-12 23:26:56 -0600

    Added loops3, modified loops2

--- Week1/loops/loops2.js
+++ Week1/loops/loops2.js
@@ -2,7 +2,7 @@ var ask = require('readline-sync');
 
 var userString = ask.question('Enter a phrase or sentence: ');
 var userCharacter = ask.question('Now enter a character (letter): ');
-for (i = 0; i < userString.length; i++) {
+for ( i = 0; i < userString.length; i++ ) {
 	if (userString[i] === userCharacter) {
 		console.log('The computer found the first instance of the character you entered (' + userCharacter + ') at position: ' + i + '.');
 		break;
@@ -13,4 +13,10 @@ if (userString.indexOf(userCharacter) === -1) {
 	console.log('Sorry. The computer could not find the character ' + userCharacter + ' in the phrase or sentence you entered.');
 }
 
-//Let me know if there's a more elegant way to code the part where you notify the user if the computer can't find the character.
+/*
+Let me know if there's a more elegant way to code the last part 
+where you notify the user if the computer can't find the character.
+I originally wanted to put the 'computer could not find the character' message 
+inside the for loop as an 'else' statement, but then in printed it out 
+for every single iteration (even when the character was in the string).
+*/
---
+++ Week1/loops/loops3.js
@@ -0,0 +1,9 @@
+var ask = require('readline-sync');
+
+var userAnswer = ask.question('How old was Eminem on April 12, 2015? ');
+
+while (userAnswer != '42') {
+	var userAnswer = ask.question('Nope! Try again: ');
+}
+
+console.log('Good job!');
